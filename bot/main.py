6d5505123ca63e4da7797efcb06d95575e255fab
Vladislav <last.chance@bk.ru>
2023-06-23 23:10:22 +0300

    Some changes

--- bot/main.py
+++ bot/main.py
@@ -1,96 +1,51 @@
 import sources
-import telepot
-from flask import request, Flask
+import telebot
 import logging
-import requests
 import config
 import random
 
-TelegramBot = telepot.Bot(config.TOKEN)
-app = Flask(__name__)
+bot = telebot.TeleBot(config.TOKEN)
 
 
-# main endpoint
-@app.route('/', methods=["GET", "POST"])
-def update():
-    if request.method == "POST" and "message" in request.json:
-        chat_id = request.json["message"]["from"]["id"]
-        try:
-            logging.info("Got POST:")
-            logging.info(request.json)
-            user_text = request.json["message"]["text"]
-            # Response for start message
-            if user_text.split(" ")[0] == "/start":
-                msg = "Available commands: \n" \
-                      "/genre Film genre \n" \
-                      "/gpt Anything \n" \
-                      "/help \n" \
-                      "/start"
-                send_message(chat_id, msg)
-
-            # Response for help message
-            elif user_text.split(" ")[0] == "/help":
-                msg = "Available commands: \n" \
-                      "/genre Film genre \n" \
-                      "/gpt Anything \n" \
-                      "/help \n" \
-                      "/start"
-                send_message(chat_id, msg)
-
-            elif user_text.split(" ")[0] == "/genre":
-                source = "Omdb"
-                description = user_text[user_text.find(" "):].lstrip()
-                msg = main(source, description)
-                logging.info("Got info from omdb:")
-                random.shuffle(msg)
-                logging.info(msg[0:6])
-                for elem in msg[0:6]:
-                    elem = elem.split("/")[2]
-                    elem_info = sources.get_info_from_omdb(elem)
-                    if ("title" and "image") in elem_info:
-                        final_msg = "Film: " + elem_info["title"] + "\n" + "Poster: " + elem_info["image"]["url"] + "\n"
-                        send_message(chat_id, final_msg)
-                    # if there is no description
-                    elif "title" in elem:
-                        final_msg = "Film: " + elem["title"] + "\n" + "There is no poster" + "\n"
-                        send_message(chat_id, final_msg)
-
-            elif user_text.split(" ")[0] == "/gpt":
-                source = "Gpt"
-                description = user_text[user_text.find(" "):].lstrip()
-                msg = main(source, description)
-                logging.info("Got info from gpt: ")
-                send_message(chat_id, msg)
-
-            # unknown message
-            elif ("/start" or "/help" or "/genre") not in user_text:
-                logging.info(user_text)
-                msg = "Unknown command, try /help"
-                send_message(chat_id, msg)
-
-            return ""
-        except Exception as e:
-            logging.info(f"Exception: ${e}")
-            send_message(chat_id, "Error occurred, probably need to add description to command.")
-            return ""
-    return ""
+@bot.message_handler(commands=['start', 'help'])
+def start_message(message):
+    logging.info(f"Got a start msg: ${message}")
+    msg = "Available commands: \n" \
+          "/genre Film genre \n" \
+          "/gpt Anything \n" \
+          "/help \n" \
+          "/start"
+    bot.send_message(message.chat.id, msg, parse_mode='Markdown')
 
 
-# send message to user
-def send_message(chat_id, text):
-    method = "sendMessage"
-    url = f"https://api.telegram.org/bot{config.TOKEN}/{method}"
-    data = {"chat_id": chat_id, "text": text}
-    requests.post(url, data=data)
+@bot.message_handler(commands=['genre'])
+def omdb_message(message):
+    logging.info(f"Got an omdb msg: ${message}")
+    description = message.text[message.text.find(" "):].lstrip()
+    msg = sources.get_info_from_omdb(description)
+    logging.info("Got info from omdb:")
+    random.shuffle(msg)
+    logging.info(msg[0:6])
+    final_msg = 'No info found'
+    for elem in msg[0:6]:
+        elem = elem.split("/")[2]
+        elem_info = sources.get_info_from_omdb(elem)
+        if ("title" and "image") in elem_info:
+            final_msg = "Film: " + elem_info["title"] + "\n" + "Poster: " + elem_info["image"]["url"] + "\n"
+        # if there is no description
+        elif "title" in elem:
+            final_msg = "Film: " + elem["title"] + "\n" + "There is no poster" + "\n"
+    bot.send_message(message.chat.id, final_msg, parse_mode='Markdown')
 
 
-def main(source, desc):
-    if source == "Omdb":
-        return sources.get_titles_from_omdb(desc=desc)
-    if source == "Gpt":
-        return sources.get_info_from_gpt(message=desc)
+@bot.message_handler(commands=['gpt'])
+def gpt_message(message):
+    logging.info(f"Got an gpt msg: ${message}")
+    description = message.text[message.text.find(" "):].lstrip()
+    msg = sources.get_info_from_gpt(description)
+    logging.info(f"Got info from gpt: ${msg}")
+    bot.send_message(message.chat.id, msg, parse_mode='Markdown')
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port='5007')
-    logging.info("App is running")
+    bot.polling(True)
